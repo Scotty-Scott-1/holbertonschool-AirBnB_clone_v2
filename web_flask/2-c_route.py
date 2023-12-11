ce04b4960e28c2d33c0efbb318707024e3dc7e99
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-11 14:34:24 +0100

    Added tasks 1, 2, 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" a module that starts an app in flask. Has 2 routes"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hello2():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello3(text):
+    new_text = text.replace("_", " ")
+    return "C {}".format(new_text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
