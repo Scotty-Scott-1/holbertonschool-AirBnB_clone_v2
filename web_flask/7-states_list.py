41c4f8d3cfd5e5c4106107af63ec07f88b6e3e4a
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-12 15:04:12 +0100

    Added task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a module that starts an app in flask. Has 2 routes"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def hello():
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
