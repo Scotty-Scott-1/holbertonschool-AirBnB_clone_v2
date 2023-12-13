47f5393ba628c0be3ee56271e73ef91bbce432f7
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 02:28:34 +0100

    updated 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def hello():
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    states = list(sorted(storage.all(State).values(), key=lambda x: x.name))
     return render_template("7-states_list.html", states=states)
 
 
