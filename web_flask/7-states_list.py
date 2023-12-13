485f5b6f15c86385f81a395def0726e6eb9b5cb6
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 01:42:59 +0100

    corrected pycodestyle for 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@ from models.state import State
 from models import storage
 
 
-
 app = Flask(__name__)
 
 
@@ -14,9 +13,11 @@ def hello():
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown(error):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
