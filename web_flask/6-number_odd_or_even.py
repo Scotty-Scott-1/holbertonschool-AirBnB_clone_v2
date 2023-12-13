850890fbe543d8529880920e98b2befebb4b535d
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 01:40:29 +0100

    Updated 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -10,14 +10,17 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello2():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello3(text):
     new_text = text.replace("_", " ")
     return "C {}".format(new_text)
+
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python", strict_slashes=False)
@@ -25,16 +28,19 @@ def hello4(text="is_cool"):
     new_text = text.replace("_", " ")
     return "Python {}".format(new_text)
 
-@app.route("/number/<n>", strict_slashes=False)
+
+@app.route("/number/<int:n>", strict_slashes=False)
 def hello5(n):
     return "{} is a number".format(int(n))
 
-@app.route("/number_template/<n>", strict_slashes=False)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def hello6(n):
     numb = int(n)
     return render_template("5-number.html", num=numb)
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def hello7(n):
     numb = int(n)
     ntype = ""
