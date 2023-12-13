eb08cfcf619379f56ee5c8912af3d383d074b574
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 12:36:41 +0100

    updated 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,8 +6,8 @@ from models.city import City
 from models import storage
 
 
-
 app = Flask(__name__)
+
 
 @app.route("/states/<id>", strict_slashes=False)
 @app.route("/states", strict_slashes=False)
@@ -16,22 +16,24 @@ def hello(id=None):
     state_exists = False
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
     if id is not None:
-        key = "state." + id
         for state in states:
             if state.id == id:
                 state_ob = state
                 state_exists = True
 
         if state_exists:
-            return render_template("9-states.html", state=state_ob, key=key, found=True)
+            return render_template("9-states.html", state=state_ob, found=True)
         else:
             return render_template("9-states.html", found=False)
     else:
-        return render_template("9-states.html", states=states, list_states=True)
+        return render_template(
+            "9-states.html", states=states, list_states=True)
+
 
 @app.teardown_appcontext
 def teardown(error):
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
