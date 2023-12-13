b1cfaf6a81c0f8e278f86d6fbdf530de9f019c14
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 01:09:12 +0100

    Corrected pycodestyle for 4, 5

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,20 +10,24 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello2():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello3(text):
     new_text = text.replace("_", " ")
     return "C {}".format(new_text)
 
+
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python", strict_slashes=False)
 def hello4(text="is_cool"):
     new_text = text.replace("_", " ")
     return "Python {}".format(new_text)
+
 
 @app.route("/number/<n>", strict_slashes=False)
 def hello5(n):
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,14 +10,17 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello2():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello3(text):
     new_text = text.replace("_", " ")
     return "C {}".format(new_text)
+
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python", strict_slashes=False)
@@ -25,9 +28,11 @@ def hello4(text="is_cool"):
     new_text = text.replace("_", " ")
     return "Python {}".format(new_text)
 
+
 @app.route("/number/<n>", strict_slashes=False)
 def hello5(n):
     return "{} is a number".format(int(n))
+
 
 @app.route("/number_template/<n>", strict_slashes=False)
 def hello6(n):
