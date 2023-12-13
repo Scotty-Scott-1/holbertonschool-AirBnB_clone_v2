ab00d72909908e098ee3a1b4336b1147d077e5f2
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-13 09:36:21 +0100

    updated 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a module that starts an app in flask. Has 2 routes"""
+""" a module that starts an app in flask. Has 1 routes"""
 from flask import Flask, render_template
 from models.state import State
 from models import storage
@@ -10,12 +10,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def hello():
+    """get a list of states"""
     states = list(sorted(storage.all(State).values(), key=lambda x: x.name))
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(error):
+    """close and reload file and db storage"""
     storage.close()
 
 
