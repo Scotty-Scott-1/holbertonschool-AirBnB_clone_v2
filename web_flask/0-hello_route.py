c2df4ea526a1dd5e7423f5867060a00173f90009
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-12-14 09:40:55 +0100

    Updated task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ a module that starts an app in flask"""
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """return hello hbnb"""
     return "Hello HBNB!"
 
 
